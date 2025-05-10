e0708b4a98ad4570d8dcc72e741ab2e020d18be9
beaverman3426 <hoibeaver@gmail.com>
2025-05-10 11:10:22 +0300

    Create predict.py
    Added a function to run the model and its encoder

---
+++ predict.py
@@ -0,0 +1,13 @@
+import joblib
+import pandas as pd
+
+
+bundle = joblib.load("model/fraud_rf_bundle.pkl")
+model = bundle['model']
+encoder = bundle['label_encoder']
+
+def predict_fraud(input_dict):
+    df = pd.DataFrame([input_dict])
+    df['type'] = encoder.transform(df['type'])
+    prediction = model.predict(df)
+    return bool(prediction[0])
